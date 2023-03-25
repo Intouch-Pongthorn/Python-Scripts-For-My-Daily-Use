77f5b5f147f83432c5f06a089210f7152edebf67
Pongthorn Chumpoo <chumpoo.dev@gmail.com>
2023-03-25 17:19:10 +0700

    finished the main functionality: write data to txt file

--- non-work/update_reading_list.py
+++ non-work/update_reading_list.py
@@ -1,8 +1,35 @@
+#!/usr/bin/env python
+import os
+from sys import argv 
 from bs4 import BeautifulSoup
 import requests
+from dotenv import load_dotenv
 
-url = input(r'enter url: ')
-page = requests.get(url)
-soup = BeautifulSoup(page.text,'html.parser')
-title = soup.title.text
-print(title)
+load_dotenv()
+location_path = os.getenv("READING_LIST_DIR_LOCATION")
+
+def main():
+    url =  argv[1]
+    title =  get_title(url)
+    write_note(title,url)
+    
+    
+def get_title(url:str)->str:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text,'html.parser')
+    title = soup.title.text
+    return title
+
+def write_note(title:str,url:str)->None:
+    #if 
+    with open(location_path,"a") as file:
+        file.write("\n")
+        file.write(f"title: {title} ")
+        file.write("\n")
+        file.write(f"url: {url} ")
+        file.write("\n")
+        file.write("#"*30)
+        file.write("\n")
+
+if __name__ == "__main__":
+    main()
