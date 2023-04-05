2b23071e24f5852d3dd2436fc9cb44584c8dc2d4
Intouch-Pongthorn <chumpoo.dev@gmail.com>
2023-04-05 17:42:28 +0700

    provided a function to create all_sar.txt

--- work-related/genalsarw.py
+++ work-related/genalsarw.py
@@ -8,36 +8,38 @@ import datetime
 def main():
     #path to the sar folder recieved from command-line 1st arg
     path = os.path.abspath(str(arg[1]))
-    #get only sarxx files from the directory
+    #path to all_sar.txt i.e. path/all_sar.txt
+    alsar_location = os.path.join(path,"all_sar.txt")
+    #fetchs only sarxx files from the directory
     sar_files = get_sar_files(path)
-
-#     try :
-#         #deletes all_sar.txt if existed 
-#         os.remove(f"{path}/all_sar.txt")
-#         #creates all_sar.txt if not existed
-#         all_sar = open(f"{path}/all_sar.txt",'a')
-#         #appends sarxx file to all_sar.txt line by line
-#         for sar_file in sar_files:
-#             current_sar_file = open(sar_file,'r')
-#             for line in current_sar_file:
-#                 all_sar.write(line)
-
-#         current_sar_file.close()
-#         all_sar.close()
-#     except OSError:
-#         pass
-#     print("complete!")
+    try : 
+        #deletes all_sar.txt if exists 
+        os.path.exists(alsar_location) and os.remove(alsar_location)
+        #opens all_sar.txt (if there's no  all_sar.txt file, this line creates the file)
+        all_sar = open(alsar_location,'a')
+        print("Writing all_sar.txt.....")
+        #appends content from each sarxx file to all_sar.txt line by line
+        for sar_file in sar_files:
+            current_sar_file = open(sar_file,'r')
+            for line in current_sar_file:
+                all_sar.write(line)
+        current_sar_file.close()
+        all_sar.close()
+    except OSError:
+        pass
+    print(f"Complete!...your all_sar.txt is already created in {alsar_location}")
 
 
 def get_sar_files(path:str)->list[str]:
-    #fetchs only sarXX.file into the list
+    #picks only sarXX.file into a list
     files_list = [file for file in glob(f"{path}\sar[0-9][0-9]")]
     #sorts sarXX.file from the list above by modified date
     files_list.sort(key = lambda file: os.stat(file).st_mtime)
-    #raises an error if sar files not found in the directory
+    #raises an error if no sar files found in the directory and ends the program
     if len(files_list) == 0:
         raise FileNotFoundError(os.strerror(errno.ENOENT),f"no sar files found in {path}")
-    #list file names and time of modification
+    print("Arranging sar files in order.......")
+    #echos file names and time of modification on the terminal screen
     for file in files_list:
         print(file, datetime.datetime.fromtimestamp(os.stat(file).st_mtime))
     
