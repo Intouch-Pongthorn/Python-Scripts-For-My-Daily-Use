dcb6579044062c237e58f76f500e0c283b7bfe94
Intouch-Pongthorn <chumpoo.dev@gmail.com>
2023-04-07 17:31:24 +0700

    edited comment

--- work-related/genalsarw.py
+++ work-related/genalsarw.py
@@ -10,15 +10,15 @@ def main():
     path = os.path.abspath(str(arg[1]))
     #path to all_sar.txt i.e. path/all_sar.txt
     alsar_location = os.path.join(path,"all_sar.txt")
-    #fetchs only sarxx files from the directory
+    #fetch only sarxx files from the directory
     sar_files = get_sar_files(path)
     try : 
-        #deletes all_sar.txt if exists 
+        #delete all_sar.txt if exists 
         os.path.exists(alsar_location) and os.remove(alsar_location)
-        #opens all_sar.txt (if there's no  all_sar.txt file, this line creates the file)
+        #open all_sar.txt (if there's no  all_sar.txt file, this line creates the file)
         all_sar = open(alsar_location,'a')
         print("Writing all_sar.txt.....")
-        #appends content from each sarxx file to all_sar.txt line by line
+        #append content from each sarxx file to all_sar.txt line by line
         for sar_file in sar_files:
             current_sar_file = open(sar_file,'r')
             for line in current_sar_file:
@@ -31,15 +31,15 @@ def main():
 
 
 def get_sar_files(path:str)->list[str]:
-    #picks only sarXX.file into a list
+    #pick only sarXX.file into a list
     files_list = [file for file in glob(f"{path}\sar[0-9][0-9]")]
-    #sorts sarXX.file from the list above by modified date
+    #sort sarXX.file from the list above by modified date
     files_list.sort(key = lambda file: os.stat(file).st_mtime)
-    #raises an error if no sar files found in the directory and ends the program
+    #raise an error if no sar files found in the directory and ends the program
     if len(files_list) == 0:
         raise FileNotFoundError(os.strerror(errno.ENOENT),f"no sar files found in {path}")
     print("Arranging sar files in order.......")
-    #echos file names and time of modification on the terminal screen
+    #echo file names and time of modification on the terminal screen
     for file in files_list:
         print(file, datetime.datetime.fromtimestamp(os.stat(file).st_mtime))
     
